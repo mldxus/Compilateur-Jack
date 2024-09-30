0b0f57baf3ce171ec7e730aae6e2180fa6852e4d
Villain Maxime <maxime.villain1@etu-univ-orleans.fr>
2024-09-30 11:15:29 +0200

    add projet

---
+++ Projet_SMT/VMTranslator/VMTranslator/Reader.py
@@ -0,0 +1,62 @@
+"""Affiche '-----debut'
+Affiche le tableau crée par la class Reader
+Affiche '-----fin'
+"""
+
+import os
+import sys
+
+
+class Reader:
+    """Crée le nombre de ligne et de colonne"""
+
+    def __init__(self, file):
+        self.char = None
+        self._line = 1
+        self._col = 1
+        if os.path.exists(file):
+            self.file = open(file, "r")
+            self.char = self.file.read(1)
+
+    def look(self):
+        """Retourne le nombre de ligne, de le colonne et de character"""
+        return {'line': self._line, 'col': self._col, 'char': self.char}
+
+    def next(self):
+        """Si le dernier char est un retour à la ligne : rajoute une ligne et une colonne, sinon rajoute juste une colonne"""
+        res = {'line': self._line, 'col': self._col, 'char': self.char}
+        if self.hasNext():
+            if self.char == '\n':
+                self._line += 1
+                self._col = 1
+            else:
+                self._col += 1
+            self.char = self.file.read(1)
+            if not self.hasNext():
+                self.file.close()
+        return res
+
+    def hasNext(self):
+        """Retourne faux si il n'y a plus de character, vrai sinon"""
+        if self.char:
+            return True
+        else:
+            return False
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.hasNext():
+            return self.next()
+        else:
+            raise StopIteration
+
+
+if __name__ == "__main__":
+    file = sys.argv[1]
+    print('-----debut')
+    lecteur = Reader(file)
+    for c in lecteur:
+        print(c)
+    print('-----fin')
