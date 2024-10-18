3add5eef6e6b55daa9d1871e9a62414d12b3ec42
Mel <melda.uslu@gmail.com>
2024-10-18 09:34:47 +0200

    Optimisation sub+neg

--- Projet_SMT/VMTranslator/VMTranslator/Arithmetiques.py
+++ Projet_SMT/VMTranslator/VMTranslator/Arithmetiques.py
@@ -9,8 +9,7 @@ class AriCommand :
         return f"""\t//{self.command['type']}  
     //Code assembleur de {self.command}\n
     @SP
-    M=M-1
-    A=M
+    AM=M-1
     D=M
     A=A-1
     M=D+M
@@ -24,20 +23,13 @@ class AriCommand :
     A=M
     D=M
     A=A-1
-    M=D-M
+    M=M-D
     """
 
     def commandneg(self):
         return f"""\t//{self.command['type']} 
     //Code assembleur de {self.command}\n
     @SP
-    M=M-1
-    A=M
-    D=M
-    @0
-    D=A-D
-    @SP
-    M=M-1
-    A+M
-    M+D
+    A=M-1
+    M=-M
     """
